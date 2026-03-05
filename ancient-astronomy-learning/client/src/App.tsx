dd5a13b27d2ea2edfb7ef11fd6e0d561d04d624f
liuxiaoning1201-cloud <liuxiaoning1201@gmail.com>
2026-03-05 19:03:09 +0800

    Add standalone site and switch to hash router
    Add a self-contained ancient-astronomy standalone.html for static/portable use and remove legacy/demo files. Switch client routing to hash-based navigation by renaming the Router component to AppRouter and wrapping it in wouter's Router with useHashLocation. Remove the analytics <script> from client/index.html and set vite.config.ts base to "./" to support relative paths for static hosting. Also includes miscellaneous file removals and a .DS_Store update.

--- ancient-astronomy-learning/client/src/App.tsx
+++ ancient-astronomy-learning/client/src/App.tsx
@@ -1,14 +1,15 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Chapter from "./pages/Chapter";
 
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
@@ -34,7 +35,9 @@ function App() {
       >
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router hook={useHashLocation}>
+            <AppRouter />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
